fe6a8066ff93246bb523742b26e7259e6fe78e9d
Brandon Patterson <brandnpatterson@gmail.com>
2018-05-24 10:26:11 -0500

    Update gulpfile.babel.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,8 +7,6 @@ let sourcemaps = require('gulp-sourcemaps');
 let styleLint = require('gulp-stylelint');
 let webpack = require('webpack-stream');
 
-let localhost = 'hello-dev.localhost';
-
 gulp.task('eslint', () => {
     return gulp.src(['./src/js/**/*.js', '!node_modules/**'])
         .pipe(eslint())
@@ -27,12 +25,7 @@ gulp.task('stylelint', () => {
 
 gulp.task('scripts', ['eslint'], () => {
     return gulp.src('./src/js/index.js')
-        .pipe(webpack({ 
-            devtool: 'source-map',
-            output: { 
-                filename: 'bundle.js' 
-            }
-        }))
+        .pipe(webpack(require('./webpack.config.js')))
         .pipe(gulp.dest('./public/'))
         .pipe(browserSync.stream());
 });
@@ -40,7 +33,7 @@ gulp.task('scripts', ['eslint'], () => {
 gulp.task('styles', ['stylelint'], () => {
     return gulp.src('./src/sass/**/*.scss')
         .pipe(sourcemaps.init())
-        .pipe(sass({ 
+        .pipe(sass({
             outputStyle: 'compressed' 
         }).on('error', sass.logError))
         .pipe(autoprefixer({ 
@@ -53,14 +46,13 @@ gulp.task('styles', ['stylelint'], () => {
 });
 
 gulp.task('watch', () => {
-    gulp.watch('./src/js/index.js', ['scripts']);
+    gulp.watch('./src/js/**/*.js', ['scripts']);
     gulp.watch('./src/sass/**/*.scss', ['styles']);
     gulp.watch('./index.html', browserSync.reload);
 });
 
 gulp.task('default', ['scripts', 'styles', 'watch'], () => {
     browserSync.init({
-        // proxy: localhost,
         server: {
             baseDir: './'
         }
